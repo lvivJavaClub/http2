9cd7d473eeee280c30af73f56ed6c89b2cc488d0
Ihor Banadiga <banadiga@gmail.com>
2018-02-28 10:09:50 +0200

    add RedirectViewController

--- src/main/java/com/lohika/jclub/http2/WebConfig.java
+++ src/main/java/com/lohika/jclub/http2/WebConfig.java
@@ -3,6 +3,7 @@ package com.lohika.jclub.http2;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -15,5 +16,10 @@ public class WebConfig extends WebMvcConfigurerAdapter {
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+  }
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addRedirectViewController("/", "index.html");
   }
 }
